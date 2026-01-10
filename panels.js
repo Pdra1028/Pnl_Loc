47ef60bb221b33b458bd13fcbe24c7b8cf4a93bd
Pdra1028 <Pdra1028@users.noreply.github.com>
2026-01-10 19:53:39 +0900

    Update Sub/Inv data and floor images

--- panels.js
+++ panels.js
@@ -1,11 +1,489 @@
-﻿window.PANELS = [
-  { name: "1번판넬", floor: "1F", x: 0.40, y: 0.60 },
-  { name: "2번판넬", floor: "1F", x: 0.20, y: 0.25 },
-  { name: "A-12",   floor: "2F", x: 0.70, y: 0.35 },
-  { name: "B-07",   floor: "2F", x: 0.35, y: 0.80 }
+﻿window.Sub = [
+  {
+    "SubKey": "B1F-001",
+    "Sub": "SC1E30000",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "1번"
+    ]
+  },
+  {
+    "SubKey": "B1F-002",
+    "Sub": "SC1F25400",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "2번"
+    ]
+  },
+  {
+    "SubKey": "B1F-003",
+    "Sub": "SC1F25530",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "3번"
+    ]
+  },
+  {
+    "SubKey": "B1F-004",
+    "Sub": "SC1F21590",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "4번"
+    ]
+  },
+  {
+    "SubKey": "B1F-005",
+    "Sub": "SC1F20770",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "5번"
+    ]
+  },
+  {
+    "SubKey": "B1F-006",
+    "Sub": "SC1F25740",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "6번"
+    ]
+  },
+  {
+    "SubKey": "B1F-007",
+    "Sub": "누전_SC1F25740",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "7번"
+    ]
+  },
+  {
+    "SubKey": "B1F-008",
+    "Sub": "SC1F26110",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "8번"
+    ]
+  },
+  {
+    "SubKey": "B1F-009",
+    "Sub": "누전_SC1F26110",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "9번"
+    ]
+  },
+  {
+    "SubKey": "B1F-010",
+    "Sub": "SC1F26560",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "10번"
+    ]
+  },
+  {
+    "SubKey": "B1F-011",
+    "Sub": "SC1F21310",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "11번"
+    ]
+  },
+  {
+    "SubKey": "B1F-012",
+    "Sub": "SC1F21180",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "12번"
+    ]
+  },
+  {
+    "SubKey": "B1F-013",
+    "Sub": "SC1F25990",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "13번"
+    ]
+  },
+  {
+    "SubKey": "B1F-014",
+    "Sub": "SC1F20610",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "14번"
+    ]
+  },
+  {
+    "SubKey": "B1F-015",
+    "Sub": "SC1F22160",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "15번"
+    ]
+  },
+  {
+    "SubKey": "B1F-016",
+    "Sub": "SC1F21880",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "16번"
+    ]
+  },
+  {
+    "SubKey": "B1F-017",
+    "Sub": "SC1F21780",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "17번"
+    ]
+  },
+  {
+    "SubKey": "B1F-018",
+    "Sub": "SC1F25920",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "18번"
+    ]
+  },
+  {
+    "SubKey": "B1F-019",
+    "Sub": "누전_SC1F25920",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "19번"
+    ]
+  },
+  {
+    "SubKey": "B1F-020",
+    "Sub": "SC1F26500",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "20번"
+    ]
+  },
+  {
+    "SubKey": "B1F-021",
+    "Sub": "누전_SC1F28070",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "21번"
+    ]
+  },
+  {
+    "SubKey": "B1F-022",
+    "Sub": "SC1F28070",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "22번"
+    ]
+  },
+  {
+    "SubKey": "B1F-023",
+    "Sub": "SC1F20440",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "23번"
+    ]
+  },
+  {
+    "SubKey": "B1F-024",
+    "Sub": "SC1F22690A",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "24번"
+    ]
+  },
+  {
+    "SubKey": "B1F-025",
+    "Sub": "SC1F22590",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "25번"
+    ]
+  },
+  {
+    "SubKey": "B1F-026",
+    "Sub": "SC1F26410",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "26번"
+    ]
+  },
+  {
+    "SubKey": "B1F-027",
+    "Sub": "SC1F23350A",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "27번"
+    ]
+  },
+  {
+    "SubKey": "B1F-028",
+    "Sub": "SC1F23270",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "28번"
+    ]
+  },
+  {
+    "SubKey": "B1F-029",
+    "Sub": "SC1F20280",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "29번"
+    ]
+  },
+  {
+    "SubKey": "B1F-030",
+    "Sub": "SC1F29000",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "30번"
+    ]
+  },
+  {
+    "SubKey": "B1F-031",
+    "Sub": "누전_SC1F28110",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "31번"
+    ]
+  },
+  {
+    "SubKey": "B1F-032",
+    "Sub": "SC1F28110",
+    "floor": "B1F-G",
+    "x": 0,
+    "y": 0,
+    "aliases": [
+      "32번"
+    ]
+  }
+];
+
+window.Inv = [
+  {
+    "Inv": "P1F21320",
+    "SubKey": "B1F-011",
+    "digits": "121320"
+  },
+  {
+    "Inv": "P1F21321",
+    "SubKey": "B1F-011",
+    "digits": "121321"
+  },
+  {
+    "Inv": "P1F25400",
+    "SubKey": "B1F-002",
+    "digits": "125400"
+  },
+  {
+    "Inv": "P1F25480",
+    "SubKey": "B1F-002",
+    "digits": "125480"
+  },
+  {
+    "Inv": "P1F25500",
+    "SubKey": "B1F-002",
+    "digits": "125500"
+  },
+  {
+    "Inv": "P1F25510",
+    "SubKey": "B1F-002",
+    "digits": "125510"
+  },
+  {
+    "Inv": "P1F25520",
+    "SubKey": "B1F-002",
+    "digits": "125520"
+  },
+  {
+    "Inv": "P1F25530",
+    "SubKey": "B1F-003",
+    "digits": "125530"
+  },
+  {
+    "Inv": "P1F25550",
+    "SubKey": "B1F-003",
+    "digits": "125550"
+  },
+  {
+    "Inv": "P1F25690",
+    "SubKey": "B1F-003",
+    "digits": "125690"
+  },
+  {
+    "Inv": "P1F25710",
+    "SubKey": "B1F-003",
+    "digits": "125710"
+  },
+  {
+    "Inv": "P1F25720",
+    "SubKey": "B1F-003",
+    "digits": "125720"
+  },
+  {
+    "Inv": "P1F25730",
+    "SubKey": "B1F-003",
+    "digits": "125730"
+  },
+  {
+    "Inv": "P1F25740",
+    "SubKey": "B1F-006",
+    "digits": "125740"
+  },
+  {
+    "Inv": "P1F25760",
+    "SubKey": "B1F-006",
+    "digits": "125760"
+  },
+  {
+    "Inv": "P1F25880",
+    "SubKey": "B1F-006",
+    "digits": "125880"
+  },
+  {
+    "Inv": "P1F25900",
+    "SubKey": "B1F-006",
+    "digits": "125900"
+  },
+  {
+    "Inv": "P1F25910",
+    "SubKey": "B1F-006",
+    "digits": "125910"
+  },
+  {
+    "Inv": "P1F26270",
+    "SubKey": "B1F-008",
+    "digits": "126270"
+  },
+  {
+    "Inv": "P1F26280",
+    "SubKey": "B1F-008",
+    "digits": "126280"
+  },
+  {
+    "Inv": "P1F26290",
+    "SubKey": "B1F-008",
+    "digits": "126290"
+  },
+  {
+    "Inv": "P1F26300",
+    "SubKey": "B1F-008",
+    "digits": "126300"
+  },
+  {
+    "Inv": "P1F26310",
+    "SubKey": "B1F-008",
+    "digits": "126310"
+  },
+  {
+    "Inv": "P1F26320",
+    "SubKey": "B1F-008",
+    "digits": "126320"
+  },
+  {
+    "Inv": "P1F26660",
+    "SubKey": "B1F-010",
+    "digits": "126660"
+  },
+  {
+    "Inv": "P1F26670",
+    "SubKey": "B1F-010",
+    "digits": "126670"
+  },
+  {
+    "Inv": "P1F26700",
+    "SubKey": "B1F-010",
+    "digits": "126700"
+  },
+  {
+    "Inv": "P1F26710",
+    "SubKey": "B1F-010",
+    "digits": "126710"
+  },
+  {
+    "Inv": "P1F26720",
+    "SubKey": "B1F-010",
+    "digits": "126720"
+  },
+  {
+    "Inv": "P1F26725",
+    "SubKey": "B1F-010",
+    "digits": "126725"
+  },
+  {
+    "Inv": "P1F30080",
+    "SubKey": "B1F-001",
+    "digits": "130080"
+  }
 ];
 
 window.FLOORS = {
-  "1F": "floors/1F.svg",
-  "2F": "floors/2F.svg"
+  "B1F": "floors/B1F.png",
+  "B1M": "floors/B1M.png",
+  "2FM": "floors/2FM.png",
+  "3FM": "floors/3FM.png",
+  "4F":  "floors/4F.png"
 };
+
